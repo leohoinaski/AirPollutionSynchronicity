dfbbd9fc6cb034efb1eda0b7d4dcc19b28aa0e97
Leonardo Hoinaski <leonardo.hoinaski@ufsc.br>
2023-11-30 10:01:10 -0300

    initialcommit

---
+++ scripts/main.py
@@ -0,0 +1,242 @@
+
+import os
+import numpy as np
+#import matplotlib.pyplot as plt
+import geopandas as gpd
+#from datetime import datetime
+import netCDF4 as nc
+import pandas as pd
+import temporalStatistics as tst
+import GAr_figs as garfig
+import matplotlib
+import shutil
+
+#%% INPUTS
+
+path = '/mnt/sdb1/AticlesProduction/Sincronization'
+fileType='BRAIN_ClippedCONC'
+borderShape = '/mnt/sdb1/shapefiles/Brasil.shp'
+cityShape='/mnt/sdb1/shapefiles/BR_Municipios_2020.shp'
+
+
+# Trim domain
+left = 5
+right = 5
+top=5
+bottom=5
+
+# left = 0
+# right = 0
+# top=0
+# bottom=0
+
+
+#%%
+NO2 = {
+  "Pollutant": "$NO_{2}$",
+  "Criteria": 0.097414,
+  "Unit": 'ppm',
+  "Criteria_annual": 0.021266,
+  "Criteria_average": '1-h average',
+  "tag":'NO2',
+  "Criteria_ave": 1,
+}
+
+CO = {
+  "Pollutant": "CO",
+  "Criteria": 9,
+  "Unit": 'ppm',
+  "Criteria_average": '8-h moving average',
+  "tag":'CO',
+  "Criteria_ave": 8,
+}
+
+O3 = {
+  "Pollutant": "$O_{3}$",
+  "Criteria":0.050961,
+  "Unit": 'ppm',
+  "Criteria_average": '8-h moving average',
+  "Criteria_ave": 8,
+  "tag":'O3'
+}
+
+SO2 = {
+  "Pollutant": "$SO_{2}$",
+  "Criteria": 0.007636,
+  "Unit": 'ppm',
+  "Criteria_annual": 0.007636,
+  "Criteria_average": '24-h average',
+  "Criteria_ave": 24,
+  "tag":'SO2'
+}
+
+PM10 = {
+  "Pollutant": "$PM_{10}$",
+  "Criteria": 50,
+  "Unit": '$\u03BCg.m^{-3}$',
+  "Criteria_annual": 20,
+  "Criteria_average": '24-h average',
+  "tag":'PM10',
+  "Criteria_ave": 24,
+}
+
+PM25 = {
+  "Pollutant": "$PM_{2.5}$",
+  "Criteria": 25,
+  "Unit": '$\u03BCg.m^{-3}$',
+  "Criteria_annual": 10,
+  "Criteria_average": '24-h average',
+  "tag":'PM25',
+  "Criteria_ave": 24,
+}
+
+pollutants = [O3]
+#%% Opening data 
+print('--------------Start GAr_outAnalysis.py------------')
+
+# Moving to dir
+os.chdir(path)
+print('Creating folders')
+
+figfolder=path+'/figures'
+if os.path.isdir(figfolder)==0:
+    os.mkdir(figfolder)
+else:
+    shutil.rmtree(figfolder)
+    os.mkdir(figfolder)
+
+
+
+print('Looping for each variable')
+for pol in pollutants:
+    
+    # Selecting files and variables
+    prefixed = sorted([filename for filename in os.listdir(path) if filename.startswith(fileType+'_'+pol['tag'])])
+
+    print('Openning netCDF files')
+    # Opening netCDF files
+    ds = nc.Dataset(path+'/'+prefixed[0])
+    
+    
+    print(pol)
+    # Selecting variable
+    if pol==PM10:
+        ATOTI = ds['ASO4I'][:]+ ds['ANO3I'][:]+ ds['ANH4I'][:]+ \
+            ds['ANAI'][:]+ ds['ACLI'][:]+ ds['AECI'][:]+ \
+                ds['AOTHRI'][:]
+                
+        ATOTJ = ds['ASO4J'][:]+ ds['ANO3J'][:]+ ds['ANH4J'][:]+ \
+            ds['ANAJ'][:]+ ds['ACLJ'][:]+ ds['AECJ'][:]+ \
+                ds['AOTHRJ'][:] + ds['AFEJ'][:]+ \
+                    ds['ASIJ'][:]+ ds['ATIJ'][:] + ds['ACAJ'][:]+ \
+                        ds['AMGJ'][:]+ ds['AMNJ'][:] + ds['AALJ'][:]+ \
+                            ds['AKJ'][:]
+
+        ATOTK = ds['ASOIL'][:]+ ds['ACORS'][:]+ ds['ASEACAT'][:]+ \
+            ds['ACLK'][:]+ ds['ASO4K'][:]+ ds['ANO3K'][:]+ \
+                ds['ANH4K'][:]
+        #REVISAR ISTO
+        data=ATOTI*1+ATOTJ*1+ATOTK*0.5
+        
+    elif pol==PM25:
+        ATOTI = ds['ASO4I'][:]+ ds['ANO3I'][:]+ ds['ANH4I'][:]+ \
+            ds['ANAI'][:]+ ds['ACLI'][:]+ ds['AECI'][:]+ \
+                ds['AOTHRI'][:]
+                
+        ATOTJ = ds['ASO4J'][:]+ ds['ANO3J'][:]+ ds['ANH4J'][:]+ \
+            ds['ANAJ'][:]+ ds['ACLJ'][:]+ ds['AECJ'][:]+ \
+                ds['AOTHRJ'][:] + ds['AFEJ'][:]+ \
+                    ds['ASIJ'][:]+ ds['ATIJ'][:] + ds['ACAJ'][:]+ \
+                        ds['AMGJ'][:]+ ds['AMNJ'][:] + ds['AALJ'][:]+ \
+                            ds['AKJ'][:]
+
+        ATOTK = ds['ASOIL'][:]+ ds['ACORS'][:]+ ds['ASEACAT'][:]+ \
+            ds['ACLK'][:]+ ds['ASO4K'][:]+ ds['ANO3K'][:]+ \
+                ds['ANH4K'][:]
+         #REVISAR ISTO   
+        data=ATOTI*1+ATOTJ*1+ATOTK*0.1
+        
+    
+    else:
+        data = ds[pol['tag']][:]
+    
+    # Get datesTime and removing duplicates
+    datesTime, data = tst.getTime(ds,data)
+    datesTimeAll = datesTime.copy()
+    
+    # Get coordinates from ioapi 
+    xv,yv,lon,lat = tst.ioapiCoords(ds)
+    xv = ds['LON'][:]
+    yv = ds['LAT'][:]
+    
+    # Trim borders left/right/bottom/top
+    dataT,xvT,yvT= tst.trimBorders(data,xv,yv,left,right,top,bottom)
+    
+    if pol['Criteria_ave']==1:
+        aveData = dataT.copy()
+    elif pol['Criteria_ave']==8:
+        # Daily-maximum 8h-moving average
+        aveData = tst.movingAverage(datesTime,dataT,8)
+        datesTime = datesTime.groupby(by=['year', 'month', 'day']).size().reset_index()
+        datesTime['datetime']=pd.to_datetime(datesTime[['year', 'month', 'day']])
+    elif pol['Criteria_ave']==24:
+        # Daily averages
+        aveData, dailyData = tst.dailyAverage(datesTime,dataT)
+        datesTime = datesTime.groupby(by=['year', 'month', 'day']).size().reset_index()
+        datesTime['datetime']=pd.to_datetime(datesTime[['year', 'month', 'day']])           
+    # Monthly averages
+    #monthlyData = tst.monthlyAverage(datesTime,dataT)
+    
+    # Frequency of violations
+    freqExcd,dataTreshold,dataBin = tst.exceedance(aveData,pol['Criteria'])
+    
+    # Transforming mercator to latlon/degrees
+    #xlon, ylat = tst.eqmerc2latlon(ds,xvT,yvT)
+    xlon, ylat = xvT,yvT
+
+    # Figures
+    # Average
+    legend = pol['Criteria_average'] + ' ' +pol['Pollutant'] +' ('+ pol['Unit'] + ')'
+    #cmap = 'YlOrRd'
+    cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ["azure","lightgray","crimson","darkred"])
+    garfig.timeAverageFig(aveData.max(axis=0)[0,:,:],xlon,ylat,legend,cmap,
+                          borderShape,figfolder,pol['tag'],pol['Criteria_average'])
+    
+    # Exceedence
+    legend2 = pol['Criteria_average'] +' ' + pol['Pollutant'] + ' - violations'
+    #cmap = 'RdPu'
+    cmap2 = matplotlib.colors.LinearSegmentedColormap.from_list("", ["azure","lightgray","salmon","red","purple"])
+    garfig.exceedanceFig(freqExcd[0,:,:],xlon,ylat,legend2,cmap2,borderShape,
+                         figfolder,pol['tag'],pol['Criteria_average'])
+    
+    # Criteria
+    
+    cmap3 = matplotlib.colors.LinearSegmentedColormap.from_list("", ["azure","lightgray","salmon","brown"])
+    #cmap = 'YlOrBr'     
+    garfig.criteriaFig(aveData.max(axis=0)[0,:,:],xlon,ylat,legend,cmap3,
+                       borderShape,pol['Criteria'],
+                       figfolder,pol['tag'],pol['Criteria_average'])
+    
+   # Yearly averages
+    if "Criteria_annual" in pol:
+        yearlyData = tst.yearlyAverage(datesTimeAll,dataT)
+        # Frequency of violations
+        freqExcdY= tst.exceedance(yearlyData,pol['Criteria_annual'])
+        legend3 = 'Annual average ' + pol['Pollutant'] + '('+ pol['Unit']+')'
+        cmap4 = matplotlib.colors.LinearSegmentedColormap.from_list("", ["azure","lightgray","crimson","darkred"])        
+        garfig.timeAverageFig(yearlyData.max(axis=0)[0,:,:],xlon,ylat,legend3,
+                              cmap4,borderShape,
+                              figfolder,pol['tag'],'Annual average')
+        # Exceedence
+        legend4 = 'Annual average ' + pol['Pollutant'] + ' - violations'
+        cmap5 = matplotlib.colors.LinearSegmentedColormap.from_list("", ["azure","lightgray","salmon","red","purple"])
+        garfig.exceedanceFig(freqExcdY[0,:,:],xlon,ylat,legend4,
+                             cmap5,borderShape,
+                             figfolder,pol['tag'],'Annual average')
+        # Criteria
+        cmap6 = matplotlib.colors.LinearSegmentedColormap.from_list("", ["azure","lightgray","salmon","brown"])    
+        garfig.criteriaFig(yearlyData.max(axis=0)[0,:,:],xlon,ylat,legend3,
+                           cmap6,borderShape,pol['Criteria_annual'],
+                           figfolder,pol['tag'],'Annual average')
+        
+    tst.spatialCluster(aveData,xlon,ylat,datesTime)
---
+++ scripts/temporalStatistics.py
@@ -0,0 +1,420 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  9 17:49:55 2022
+
+@author: leohoinaski
+"""
+# import os
+import numpy as np
+from datetime import datetime
+import pandas as pd
+# import netCDF4 as nc
+# from numpy.lib.stride_tricks import sliding_window_view
+import pyproj
+from shapely.geometry import Point
+import geopandas as gpd
+from ismember import ismember
+# import wrf
+
+
+def dailyAverage(datesTime, data):
+    if len(data.shape) > 3:
+        daily = datesTime.groupby(['year', 'month', 'day']).count()
+        dailyData = np.empty(
+            (daily.shape[0], data.shape[1], data.shape[2], data.shape[3]))
+        for day in range(0, daily.shape[0]):
+            findArr = (datesTime['year'] == daily.index[day][0]) & \
+                (datesTime['month'] == daily.index[day][1]) & \
+                (datesTime['day'] == daily.index[day][2])
+            dailyData[day, :, :, :] = data[findArr, :, :, :].mean(axis=0)
+    else:
+        daily = datesTime.groupby(['year', 'month', 'day']).count()
+        dailyData = np.empty((daily.shape[0], data.shape[1], data.shape[2]))
+        for day in range(0, daily.shape[0]):
+            findArr = (datesTime['year'] == daily.index[day][0]) & \
+                (datesTime['month'] == daily.index[day][1]) & \
+                (datesTime['day'] == daily.index[day][2])
+            dailyData[day, :, :] = data[findArr, :, :].mean(axis=0)
+    daily = daily.reset_index()
+    return dailyData, daily
+
+
+def dailyRainWRF(datesTime, data):
+    if len(data.shape) > 3:
+        daily = datesTime.groupby(['year', 'month', 'day']).count()
+        dailyData = np.empty(
+            (daily.shape[0], data.shape[1], data.shape[2], data.shape[3]))
+        for day in range(0, daily.shape[0]):
+            findArr = (datesTime['year'] == daily.index[day][0]) & \
+                (datesTime['month'] == daily.index[day][1]) & \
+                (datesTime['day'] == daily.index[day][2])
+            # findArr=findArr.reset_index()
+            findArr[np.where(findArr)[0][:-1]] = False
+            dailyData[day, :, :, :] = data[findArr, :, :, :]
+    else:
+        daily = datesTime.groupby(['year', 'month', 'day']).count()
+        dailyData = np.empty((daily.shape[0], data.shape[1], data.shape[2]))
+        for day in range(0, daily.shape[0]):
+            findArr = (datesTime['year'] == daily.index[day][0]) & \
+                (datesTime['month'] == daily.index[day][1]) & \
+                (datesTime['day'] == daily.index[day][2])
+            findArr[np.where(findArr)[0][:-1]] = False
+            dailyData[day, :, :] = data[findArr, :, :]
+    daily = daily.reset_index()
+    return dailyData, daily
+
+
+def monthlyAverage(datesTime, data):
+    monthly = datesTime.groupby(['year', 'month']).count()
+    monthlyData = np.empty(
+        (monthly.shape[0], data.shape[1], data.shape[2], data.shape[3]))
+    for month in range(0, monthly.shape[0]):
+        findArr = (datesTime['year'] == monthly.index[month][0]) & \
+            (datesTime['month'] == monthly.index[month][1])
+        monthlyData[month, :, :, :] = data[findArr, :, :, :].mean(axis=0)
+
+    return monthlyData
+
+
+def yearlyAverage(datesTime, data):
+    if len(data.shape) > 3:
+        yearly = datesTime.groupby(['year']).count()
+        yearlyData = np.empty(
+            (yearly.shape[0], data.shape[1], data.shape[2], data.shape[3]))
+        for year in range(0, yearly.shape[0]):
+            if yearly.shape[0] > 1:
+                findArr = (datesTime['year'] == yearly.index[year])
+            else:
+                findArr = (datesTime['year'] == yearly.index[year])
+            yearlyData[year, :, :, :] = data[findArr, :, :, :].mean(axis=0)
+    else:
+        yearly = datesTime.groupby(['year']).count()
+        yearlyData = np.empty((yearly.shape[0], data.shape[1], data.shape[2]))
+        for year in range(0, yearly.shape[0]):
+            if yearly.shape[0] > 1:
+                findArr = (datesTime['year'] == yearly.index[year])
+            else:
+                findArr = (datesTime['year'] == yearly.index[year])
+            yearlyData[year, :, :] = data[findArr, :, :].mean(axis=0)
+    return yearlyData
+
+
+def yearlySum(datesTime, data):
+    if len(data.shape) > 3:
+        yearly = datesTime.groupby(['year']).count()
+        yearlyData = np.empty(
+            (yearly.shape[0], data.shape[1], data.shape[2], data.shape[3]))
+        for year in range(0, yearly.shape[0]):
+            if yearly.shape[0] > 1:
+                findArr = (datesTime['year'] == yearly.index[year])
+            else:
+                findArr = (datesTime['year'] == yearly.index[year])
+            yearlyData[year, :, :, :] = data[findArr, :, :, :].sum(axis=0)
+    else:
+        yearly = datesTime.groupby(['year']).count()
+        yearlyData = np.empty((yearly.shape[0], data.shape[1], data.shape[2]))
+        for year in range(0, yearly.shape[0]):
+            if yearly.shape[0] > 1:
+                findArr = (datesTime['year'] == yearly.index[year])
+            else:
+                findArr = (datesTime['year'] == yearly.index[year])
+            yearlyData[year, :, :] = data[findArr, :, :].sum(axis=0)
+
+    return yearlyData
+
+
+def movingAverage(datesTime, data, w):
+    daily = datesTime.groupby(['year', 'month', 'day']).count()
+    mvAveData = np.empty(
+        (daily.shape[0], data.shape[1], data.shape[2], data.shape[3]))
+    for day in range(0, daily.shape[0]):
+        findArr = (datesTime['year'] == daily.index[day][0]) & \
+            (datesTime['month'] == daily.index[day][1]) & \
+            (datesTime['day'] == daily.index[day][2])
+        for ii in range(0, findArr.sum()):
+            ddData = data[findArr, :, :, :]
+            if w+ii <= findArr.sum():
+                dataN = ddData[ii:w+ii, :, :, :].mean(axis=0)
+                if ii == 0:
+                    movData = dataN
+                else:
+                    movData = np.max([movData, dataN], axis=0)
+        mvAveData[day, :, :, :] = movData
+    return mvAveData
+
+
+def datePrepCMAQ(ds):
+    tf = np.array(ds['TFLAG'][:])
+    print(tf)
+    date = []
+    for ii in range(0, tf.shape[0]):
+        date.append(datetime.strptime(tf.astype(str)[
+                    ii], '%Y%m%d%H').strftime('%Y-%m-%d %H:00:00'))
+
+    date = np.array(date, dtype='datetime64[s]')
+    dates = pd.DatetimeIndex(date)
+    datesTime = pd.DataFrame()
+    datesTime['year'] = dates.year
+    datesTime['month'] = dates.month
+    datesTime['day'] = dates.day
+    datesTime['hour'] = dates.hour
+    datesTime['datetime'] = dates
+    return datesTime
+
+# def datePrepWRF(ds):
+#     date = np.array(wrf.g_times.get_times(ds,timeidx=wrf.ALL_TIMES))
+#     dates = pd.DatetimeIndex(date)
+#     datesTime=pd.DataFrame()
+#     datesTime['year'] = dates.year
+#     datesTime['month'] = dates.month
+#     datesTime['day'] = dates.day
+#     datesTime['hour'] = dates.hour
+#     datesTime['datetime']=dates
+#     return datesTime
+
+
+def ioapiCoords(ds):
+    # Latlon
+    lonI = ds.XORIG
+    latI = ds.YORIG
+
+    # Cell spacing
+    xcell = ds.XCELL
+    ycell = ds.YCELL
+    ncols = ds.NCOLS
+    nrows = ds.NROWS
+
+    lon = np.arange(lonI, (lonI+ncols*xcell), xcell)
+    lat = np.arange(latI, (latI+nrows*ycell), ycell)
+
+    xv, yv = np.meshgrid(lon, lat)
+    return xv, yv, lon, lat
+
+
+def exceedance(data, criteria):
+    freqExcd = np.sum(data > criteria, axis=0)
+    dataTreshold = data.copy()
+    dataTreshold[dataTreshold < criteria] = np.nan
+    dataBin = data.copy()
+    dataBin[dataBin < criteria] = 0
+    dataBin[dataBin > criteria] = 1
+
+    return freqExcd, dataTreshold, dataBin
+
+
+def exceedanceSinc(dataBin, xlon, ylat):
+    dataV = np.argwhere(np.sum(dataBin[:, 0, :, :], axis=0) > 0)
+    dataTr = []
+    for ii in dataV:
+        print(ii)
+        dataTr.append(dataBin[:, 0, ii[0], ii[1]])
+    dataTrArr = np.array(dataTr)
+
+    con = []
+    conXY=[]
+    for ii in range(0, dataTrArr.shape[0]):
+        print(ii)
+        for jj in range(0, dataTrArr.shape[0]):
+            corrmat = np.corrcoef(dataTrArr[ii, :], dataTrArr[jj, :])
+            if ii!=jj:
+                if corrmat[0, 1] > 0.7:
+                    con.append([dataV[ii,0],dataV[ii,1], dataV[jj,0],dataV[jj,1]])
+                    conXY.append([xlon[dataV[ii,0],dataV[ii,1]],
+                                 ylat[dataV[ii,0],dataV[ii,1]], 
+                                 xlon[dataV[jj,0],dataV[jj,1]],
+                                 ylat[dataV[jj,0],dataV[jj,1]]])
+                    
+    return conXY
+
+def spatialCluster(aveData,xlon,ylat,datesTime):
+    import cgc
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import xarray as xr
+    
+    
+    from cgc.triclustering import Triclustering
+    from cgc.kmeans import KMeans
+    
+    spring_indices = xr.DataArray(
+        data=aveData[:,:,:,:],
+        dims=["time","spring_index","y", "x"],
+        coords=dict(
+            x=(["x"], xlon[0,:]),
+            y=(["y"], ylat[:,0]),
+            time=datesTime.datetime,
+            spring_index=['O3'],
+        ),
+        attrs=dict(
+            description="concentration",
+            #units="degC",
+        ),
+    )
+
+    print(spring_indices)
+
+    
+    spring_indices = spring_indices.stack(space=['x', 'y'])
+    location = np.arange(spring_indices.space.size) # create a combined (x,y) index
+    spring_indices = spring_indices.assign_coords(location=('space', location))
+    
+    # drop pixels that are null-valued for any year/spring-index
+    #spring_indices = spring_indices.dropna('space', how='any')  
+    print(spring_indices)
+
+
+    num_band_clusters = 1
+    num_time_clusters = 5
+    num_space_clusters = 10
+    
+    max_iterations = 50  # maximum number of iterations
+    conv_threshold = 0.1  # convergence threshold 
+    nruns = 1  # number of differently-initialized runs
+    
+    tc = Triclustering(
+        spring_indices.data,  # data array with shape: (bands, rows, columns)
+        num_time_clusters,  
+        num_space_clusters,
+        num_band_clusters, 
+        max_iterations=max_iterations,  
+        conv_threshold=conv_threshold, 
+        nruns=nruns
+    )
+    
+    results = tc.run_with_threads(nthreads=1)
+
+    time_clusters = xr.DataArray(results.bnd_clusters, dims='time', 
+                                 coords=spring_indices.time.coords, 
+                                 name='time cluster')
+    space_clusters = xr.DataArray(results.col_clusters, dims='space', 
+                                  coords=spring_indices.space.coords, 
+                                  name='space cluster')
+    band_clusters = xr.DataArray(results.row_clusters, dims='spring_index', 
+                                 coords=spring_indices.spring_index.coords, 
+                                 name='band cluster')
+    
+    fig, ax = plt.subplots()
+
+    space_clusters_xy = space_clusters.unstack('space')
+    space_clusters_xy.isel().plot.imshow(
+        x='x', y='y', levels=range(num_space_clusters+1)
+    )
+
+    clusters = (results.bnd_clusters, results.row_clusters, results.col_clusters)
+    nclusters = (num_band_clusters, num_time_clusters, num_space_clusters)
+    km = KMeans(
+        spring_indices.data,
+        clusters=clusters,
+        nclusters=nclusters,
+        k_range=range(2, 10)
+    )
+    results_kmeans = km.compute()
+    print(f"Optimal k value: {results_kmeans.k_value}")
+    
+    labels = xr.DataArray(
+    results_kmeans.labels, 
+        coords=(
+            ('band_clusters', range(num_band_clusters)),
+            ('time_clusters', range(num_time_clusters)), 
+            ('space_clusters', range(num_space_clusters))
+        )
+    )
+    
+    # drop tri-clusters that are not populated  
+    labels = labels.dropna('band_clusters', how='all')
+    labels = labels.dropna('time_clusters', how='all')
+    labels = labels.dropna('space_clusters', how='all')
+    labels = labels.sel(space_clusters=space_clusters, drop=True)
+    labels = labels.unstack('space')
+    fig, ax = plt.subplots()
+    ax.pcolor(labels[0,0,:,:].transpose())
+    return
+
+
+def eqmerc2latlon(ds, xv, yv):
+
+    mapstr = '+proj=merc +a=%s +b=%s +lat_ts=0 +lon_0=%s' % (
+        6370000, 6370000, ds.XCENT)
+    # p = pyproj.Proj("+proj=merc +lon_0="+str(ds.P_GAM)+" +k=1 +x_0=0 +y_0=0 +a=6370000 +b=6370000 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")
+    p = pyproj.Proj(mapstr)
+    xlon, ylat = p(xv, yv, inverse=True)
+
+    return xlon, ylat
+
+
+def trimBorders(data, xv, yv, left, right, top, bottom):
+    if np.size(data.shape) == 4:
+        dataT = data[:, :, bottom:(data.shape[2]-top),
+                     left:(data.shape[3]-right)]
+        xvT = xv[bottom:(data.shape[2]-top), left:(data.shape[3]-right)]
+        yvT = yv[bottom:(data.shape[2]-top), left:(data.shape[3]-right)]
+    if np.size(data.shape) == 3:
+        dataT = data[:, bottom:(data.shape[1]-top), left:(data.shape[2]-right)]
+        xvT = xv[bottom:(data.shape[1]-top), left:(data.shape[2]-right)]
+        yvT = yv[bottom:(data.shape[1]-top), left:(data.shape[2]-right)]
+    if np.size(data.shape) == 2:
+        dataT = data[bottom:(data.shape[0]-top), left:(data.shape[1]-right)]
+        xvT = xv[bottom:(data.shape[0]-top), left:(data.shape[1]-right)]
+        yvT = yv[bottom:(data.shape[0]-top), left:(data.shape[1]-right)]
+
+    # xvT = xv[bottom:(data.shape[2]-top),left:(data.shape[3]-right)]
+    # yvT = yv[bottom:(data.shape[2]-top),left:(data.shape[3]-right)]
+
+    return dataT, xvT, yvT
+
+
+def getTime(ds, data):
+    dd = datePrepCMAQ(ds)
+    idx2Remove = np.array(dd.drop_duplicates().index)
+    data = data[idx2Remove]
+    datesTime = dd.drop_duplicates().reset_index(drop=True)
+    return datesTime, data
+
+# def getTimeWRF(ds,data):
+#     dd = datePrepWRF(ds)
+#     idx2Remove = np.array(dd.drop_duplicates().index)
+#     data = data[idx2Remove]
+#     datesTime = dd.drop_duplicates().reset_index(drop=True)
+#     return datesTime,data
+
+
+def citiesINdomain(xlon, ylat, cities):
+    s = gpd.GeoSeries(map(Point, zip(xlon.flatten(), ylat.flatten())))
+    s = gpd.GeoDataFrame(geometry=s)
+    s.crs = "EPSG:4326"
+    s.to_crs("EPSG:4326")
+    pointIn = cities.geometry.clip(s).explode()
+    pointIn = gpd.GeoDataFrame({'geometry': pointIn}).reset_index()
+    lia, loc = ismember(np.array((s.geometry.x, s.geometry.y)).transpose(),
+                        np.array((pointIn.geometry.x, pointIn.geometry.y)).transpose(), 'rows')
+    s['city'] = np.nan
+    s.iloc[lia, 1] = cities['CD_MUN'][pointIn['level_0'][loc]].values
+    cityMat = np.reshape(
+        np.array(s.city), (xlon.shape[0], xlon.shape[1])).astype(float)
+    return s, cityMat
+
+
+def dataINcity(aveData, datesTime, cityMat, s, IBGE_CODE):
+    # IBGE_CODE=4202404
+    if np.size(aveData.shape) == 4:
+        cityData = aveData[:, :, cityMat == IBGE_CODE]
+        cityDataPoints = s[s.city.astype(float) == IBGE_CODE]
+        cityData = cityData[:, 0, :]
+        matData = aveData.copy()
+        matData[:, :, cityMat != IBGE_CODE] = np.nan
+        cityDataFrame = pd.DataFrame(cityData)
+        cityDataFrame.columns = cityDataPoints.geometry.astype(str)
+        cityDataFrame['Datetime'] = datesTime.datetime
+        cityDataFrame = cityDataFrame.set_index(['Datetime'])
+    else:
+        cityData = aveData[:, cityMat == IBGE_CODE]
+        cityDataPoints = s[s.city.astype(float) == IBGE_CODE]
+        cityData = cityData[:, :]
+        matData = aveData.copy()
+        matData[:, cityMat != IBGE_CODE] = np.nan
+        cityDataFrame = pd.DataFrame(cityData)
+        cityDataFrame.columns = cityDataPoints.geometry.astype(str)
+        cityDataFrame['Datetime'] = datesTime.datetime
+        cityDataFrame = cityDataFrame.set_index(['Datetime'])
+    return cityData, cityDataPoints, cityDataFrame, matData
